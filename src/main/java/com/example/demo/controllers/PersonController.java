7e2fe02e648baf4934eab1366cefb3cb26de4471
VitorDalBelo <vitoralmeida721@gmail.com>
2024-04-20 17:08:15 -0300

    s7-a69-Verbos

--- src/main/java/com/example/demo/controllers/PersonController.java
+++ src/main/java/com/example/demo/controllers/PersonController.java
@@ -3,13 +3,16 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Person;
 import com.example.demo.services.PersonServices;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -31,6 +34,33 @@ public class PersonController {
         List<Person> personList = personServices.findAll();
         return personList;
     }
-    
+
+    @RequestMapping(
+        value="",
+        method=RequestMethod.POST,
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person) {
+        return personServices.create(person);
+    }
+
+    @RequestMapping(
+        value="",
+        method=RequestMethod.PUT,
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person) {
+        return personServices.update(person);
+    }
+    @RequestMapping(
+        value="/{id}",
+        method=RequestMethod.DELETE
+    )
+    public ResponseEntity<Void> delete(@PathVariable(value= "id") String id) {
+        personServices.delete(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
     
 }
--- src/main/java/com/example/demo/services/PersonServices.java
+++ src/main/java/com/example/demo/services/PersonServices.java
@@ -11,22 +11,22 @@ import com.example.demo.model.Person;
 
 @Service
 public class PersonServices {
-     private final AtomicLong counter = new AtomicLong();
-     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+   private final AtomicLong counter = new AtomicLong();
+   private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
-     public Person findById(String id){
-        logger.info("Finding for one person");
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("João");
-        person.setLastName("Silva");
-        person.setAddress("Rua ABC, 123");
-        person.setGender("Masculino");
+   public Person findById(String id){
+      logger.info("Finding for one person");
+      Person person = new Person();
+      person.setId(counter.incrementAndGet());
+      person.setFirstName("João");
+      person.setLastName("Silva");
+      person.setAddress("Rua ABC, 123");
+      person.setGender("Masculino");
 
-        return person;
-     }
+      return person;
+   }
 
-     public List<Person> findAll(){
+   public List<Person> findAll(){
       logger.info("Finding all people");
       List<Person> people = new ArrayList<>();
       for(int i = 0; i < 8 ; i++){
@@ -35,13 +35,30 @@ public class PersonServices {
       }
       return people;
    }
-     private Person mockPerson (int i) {
-      Person person = new Person ();
-      person. setId(counter.incrementAndGet());
-      person.setFirstName("Person name " + i);
-      person.setLastName("Last name " + i);
-      person.setAddress("Some address in Brasil" + i);
-      person. setGender (i % 2 == 0 ? "Masculino" : "Feminino");
+
+   public Person create(Person person){
+      logger.info("Creating a people");
+      
       return person;
-     }
+   }
+
+   public Person update(Person person){
+      logger.info("Updating a people");
+
+      return person;
+   }
+
+   public void delete(String id){
+      logger.info("Deleting a people");
+   }
+
+   private Person mockPerson (int i) {
+   Person person = new Person ();
+   person. setId(counter.incrementAndGet());
+   person.setFirstName("Person name " + i);
+   person.setLastName("Last name " + i);
+   person.setAddress("Some address in Brasil" + i);
+   person. setGender (i % 2 == 0 ? "Masculino" : "Feminino");
+   return person;
+   }
 }
