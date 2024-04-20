8098e8551eb26a935951d9fc8a72fc3b2cd67745
VitorDalBelo <vitoralmeida721@gmail.com>
2024-04-20 13:42:06 -0300

    s7-a68-get_endpoints

--- src/main/java/com/example/demo/controllers/PersonController.java
+++ src/main/java/com/example/demo/controllers/PersonController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Person;
@@ -18,9 +21,16 @@ public class PersonController {
     @Autowired
     private PersonServices personServices;
 
-    @RequestMapping(value="/{id}",method=RequestMethod.GET)
+    @RequestMapping(value="/{id}",method=RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id){
         return personServices.findById(id);
     }
+
+    @RequestMapping(value="", method=RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        List<Person> personList = personServices.findAll();
+        return personList;
+    }
+    
     
 }
--- src/main/java/com/example/demo/services/PersonServices.java
+++ src/main/java/com/example/demo/services/PersonServices.java
@@ -1,5 +1,7 @@
 package com.example.demo.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -22,5 +24,24 @@ public class PersonServices {
         person.setGender("Masculino");
 
         return person;
+     }
+
+     public List<Person> findAll(){
+      logger.info("Finding all people");
+      List<Person> people = new ArrayList<>();
+      for(int i = 0; i < 8 ; i++){
+         Person person = this.mockPerson(i);
+         people.add(person);
+      }
+      return people;
+   }
+     private Person mockPerson (int i) {
+      Person person = new Person ();
+      person. setId(counter.incrementAndGet());
+      person.setFirstName("Person name " + i);
+      person.setLastName("Last name " + i);
+      person.setAddress("Some address in Brasil" + i);
+      person. setGender (i % 2 == 0 ? "Masculino" : "Feminino");
+      return person;
      }
 }
